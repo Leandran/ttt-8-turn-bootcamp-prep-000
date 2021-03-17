0d37659e0bcccf2d7b5b76bab3defe17189424d7
Leandran M <websitesa95@gmail.com>
2021-03-17 22:40:53 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board,index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    return true
+  else
+    return false
+  end
+end
+
+
+
+def turn
+  puts "Please enter 1-9:"
+end
+
+def input_to_index(user_input)
+  input = user_input.to_i
+  input -= 1
+  return input
+end
+
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+   board[index] == "X" || board[index] == "O"
+   return true
+ end
+end
